974a065e5f1da10fe44302fb3044d1b0706803ed
Steel Wheels Project <wheel@tcn.zaq.ne.jp>
2019-08-15 20:27:27 +0900

    build: Initial commit of the project file

---
+++ Source/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  JSTerminal
+//
+//  Created by Tomoo Hamada on 2019/08/15.
+//  Copyright © 2019 Steel Wheels Project. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ Source/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  JSTerminal
+//
+//  Created by Tomoo Hamada on 2019/08/15.
+//  Copyright © 2019 Steel Wheels Project. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
