2c8a538de001402c029651534389bdb54d82ecb1
Steel Wheels Project <wheel@tcn.zaq.ne.jp>
2021-02-25 21:32:22 +0900

    sample: Add jungle-sim.jspkg

---
+++ Resource/Game/jungle-sim.jspkg/main.js
@@ -0,0 +1,12 @@
+/*
+ * main.js
+ */
+
+function main(args)
+{
+	console.log("Jungle Simulation !!") ;
+	let vstat = enterView("main", function(retval){
+		console.log("Result = " + retval) ;
+	}) ;
+}
+
