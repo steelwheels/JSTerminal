1856214d7c9486e8cc397ac822cf6076c7bc589c
Steel Wheels Project <wheel@tcn.zaq.ne.jp>
2021-04-10 09:51:14 +0900

    resource: Update sample script: curses.js

--- Resource/Sample/curses.js
+++ Resource/Sample/curses.js
@@ -5,7 +5,25 @@
 function main(args)
 {
 	Curses.begin() ;
-	sleep(10) ;
+
+	let width  = Curses.width ;
+	let height = Curses.height ;
+
+	Curses.fill(0, 0, width, height, "+") ;
+
+	Curses.moveTo(width/2, height/2) ;
+	console.print(`Hello, World !! ${width}x${height}`) ;
+	
+	for(let x=0 ; x<width ; x++) {
+		let v = x % 10 ;
+		Curses.moveTo(x, 0) ;
+		console.print(`${v}`) ;
+
+		Curses.moveTo(x, height - 1) ;
+		console.print(`${v}`) ;
+	}
+
+	sleep(20) ;
 	Curses.end() ;
 }
 
