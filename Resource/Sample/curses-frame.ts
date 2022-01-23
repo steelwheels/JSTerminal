a2a1b5ffce3471e6809735e8d6e6b11f8bec67f6
Steel Wheels Project <wheel@tcn.zaq.ne.jp>
2022-01-23 10:07:46 +0900

    resource: Add sample: hello-world.jspkg

---
+++ Resource/Sample/curses-frame.ts
@@ -0,0 +1,33 @@
+/*
+ * curses-frame.ts
+ */
+
+/// <reference path="types/KiwiLibrary.d.ts"/>
+
+function main(args : string[]){
+        Curses.begin() ;
+
+	let width  = Curses.width ;
+	let height = Curses.height ;
+	Curses.fill(0, 0, width, height, " ") ;
+
+        let frame = new CFrame(Rect(5, 5, 20, 10)) ;
+        frame.foregroundColor = Curses.yellow ;
+        frame.backgroundColor = Curses.blue ;
+        frame.fill(" ") ;
+        if(frame.moveTo(4, 4)){
+                frame.put("Hello, world !!") ;
+        } else {
+                frame.put("Error !!") ;
+        }
+        if(frame.moveTo(0, frame.frame.height - 1)){
+                frame.put("Press 'q' to quit") ;
+        } else {
+                frame.put("Error !!") ;
+        }
+
+        while(Curses.inkey() == null){
+                ;
+        }
+        Curses.end() ;
+}
