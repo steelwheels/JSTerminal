a22aec5ba4624b5259737e7d32144419f69eb33a
Steel Wheels Project <wheel@tcn.zaq.ne.jp>
2022-03-29 21:48:52 +0900

    resource: Add sample script: listner-1.jspkg

---
+++ Resource/Sample/listner-1.jspkg/main.js
@@ -0,0 +1,11 @@
+"use strict";
+/*
+ * main.ts
+ */
+/// <reference path="types/KiwiLibrary.d.ts"/>
+/// <reference path="types/KiwiComponent.d.ts"/>
+function main(args) {
+    console.log("Hello, world !!");
+    let retval = enterView("checkbox", null);
+    console.log("Result = " + retval);
+}
