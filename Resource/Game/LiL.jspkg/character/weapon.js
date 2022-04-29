eb9c16f9440894182fe06c567700fb6854dfe9e5
Steel Wheels Project <wheel@tcn.zaq.ne.jp>
2022-04-29 15:27:52 +0900

    resource: Update game script: LiL.jspkg

--- Resource/Game/LiL.jspkg/character/weapon.js
+++ Resource/Game/LiL.jspkg/character/weapon.js
@@ -6,19 +6,57 @@
 /// <reference path="../types/KiwiShell.d.ts" />
 /// <reference path="../types/KiwiComponent.d.ts" />
 var Weapon;
-(function (Weapon) {
+(function (Weapon_1) {
     class WeaponTable {
         constructor() {
-            let table = tableInStorage("main", "item.weapon.weapons");
+            let table = tableInStorage("main", WeaponTable.path);
             if (table != null) {
                 this.mTable = table;
             }
             else {
-                console.error("[Error] No table for item.weapon.weapons\n");
+                console.error("[Error] No table for " +
+                    WeaponTable.path + "\n");
                 this.mTable = null;
             }
         }
+        get weaponCount() {
+            let table = this.mTable;
+            if (table != null) {
+                return table.recordCount;
+            }
+            else {
+                return 0;
+            }
+        }
+        weapon(row) {
+            let table = this.mTable;
+            if (table != null) {
+                let record = table.record(row);
+                if (record != null) {
+                    return new Weapon(record);
+                }
+            }
+            return null;
+        }
     }
-    Weapon.WeaponTable = WeaponTable;
+    WeaponTable.path = "item.weapon.weapons";
+    Weapon_1.WeaponTable = WeaponTable;
+    class Weapon {
+        constructor(record) {
+            if (record != null) {
+                this.mRecord = record;
+            }
+            else {
+                this.mRecord = Record();
+            }
+        }
+        get realName() {
+            return this.mRecord.value("name_r");
+        }
+        get uncertainName() {
+            return this.mRecord.value("name_u");
+        }
+    }
+    Weapon_1.Weapon = Weapon;
 })(Weapon || (Weapon = {}));
 ; // end of module
--- Resource/Game/LiL.jspkg/types/character/weapon.d.ts
+++ Resource/Game/LiL.jspkg/types/character/weapon.d.ts
@@ -3,7 +3,16 @@
 /// <reference path="../KiwiComponent.d.ts" />
 declare module Weapon {
     class WeaponTable {
+        static path: string;
         private mTable;
         constructor();
+        get weaponCount(): number;
+        weapon(row: number): Weapon | null;
+    }
+    class Weapon {
+        private mRecord;
+        constructor(record: RecordIF | null);
+        get realName(): string | null;
+        get uncertainName(): string | null;
     }
 }
