42423d7124f91db2b7734cdbc7a381ba319ffa6f
Steel Wheels Project <wheel@tcn.zaq.ne.jp>
2022-05-12 23:45:25 +0900

    resource: Add saple script: func-1.jspkg, value-table-6.jspkg

---
+++ Resource/Sample/func-1.jspkg/main.js
@@ -0,0 +1,12 @@
+"use strict";
+/*
+ * main.ts
+ */
+/// <reference path="types/KiwiLibrary.d.ts"/>
+/// <reference path="types/KiwiComponent.d.ts"/>
+function main(args) {
+    console.print("# func-1\n");
+    let retval = enterView("main_func", null);
+    console.print("retval = " + retval + "\n");
+    return 0;
+}
---
+++ Resource/Sample/func-1.jspkg/main.ts
@@ -0,0 +1,15 @@
+/*
+ * main.ts
+ */
+
+/// <reference path="types/KiwiLibrary.d.ts"/>
+/// <reference path="types/KiwiComponent.d.ts"/>
+
+function main(args : [string])
+{
+	console.print("# func-1\n") ;
+	let retval = enterView("main_func", null) ;
+	console.print("retval = " + retval + "\n") ;
+	return 0 ;
+}
+
