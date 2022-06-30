5c7db65a4aa3e041c336e9a3cf47b41ffea8dfb1
Steel Wheels Project <wheel@tcn.zaq.ne.jp>
2022-07-01 00:14:00 +0900

    resource: Add sample script: storage-map-table0.jspkg

--- Resource/Library/types/KiwiLibrary.d.ts
+++ Resource/Library/types/KiwiLibrary.d.ts
@@ -1,3 +1,150 @@
+declare enum AccessType {
+  append = 2,
+  read = 0,
+  write = 1
+}
+declare namespace AccessType {
+  function description(param: AccessType): string ;
+  const keys: string[] ;
+}
+declare enum Alignment {
+  center = 3,
+  fill = 2,
+  leading = 0,
+  trailing = 1
+}
+declare namespace Alignment {
+  function description(param: Alignment): string ;
+  const keys: string[] ;
+}
+declare enum AnimationState {
+  idle = 0,
+  pause = 2,
+  run = 1
+}
+declare namespace AnimationState {
+  function description(param: AnimationState): string ;
+  const keys: string[] ;
+}
+declare enum Authorize {
+  authorized = 3,
+  denied = 2,
+  undetermined = 0
+}
+declare namespace Authorize {
+  function description(param: Authorize): string ;
+  const keys: string[] ;
+}
+declare enum Axis {
+  horizontal = 0,
+  vertical = 1
+}
+declare namespace Axis {
+  function description(param: Axis): string ;
+  const keys: string[] ;
+}
+declare enum ComparisonResult {
+  ascending = -1,
+  descending = 1,
+  same = 0
+}
+declare namespace ComparisonResult {
+  function description(param: ComparisonResult): string ;
+  const keys: string[] ;
+}
+declare enum Distribution {
+  equalSpacing = 3,
+  fill = 0,
+  fillEqually = 2,
+  fillProportinally = 1
+}
+declare namespace Distribution {
+  function description(param: Distribution): string ;
+  const keys: string[] ;
+}
+declare enum ExitCode {
+  commaneLineError = 2,
+  exception = 4,
+  internalError = 1,
+  noError = 0,
+  syntaxError = 3
+}
+declare namespace ExitCode {
+  function description(param: ExitCode): string ;
+  const keys: string[] ;
+}
+declare enum FileType {
+  directory = 2,
+  file = 1,
+  notExist = 0
+}
+declare namespace FileType {
+  function description(param: FileType): string ;
+  const keys: string[] ;
+}
+declare enum FontSize {
+  large = 19,
+  regular = 13,
+  small = 11
+}
+declare namespace FontSize {
+  function description(param: FontSize): string ;
+  const keys: string[] ;
+}
+declare enum LogLevel {
+  debug = 3,
+  detail = 4,
+  error = 1,
+  nolog = 0,
+  warning = 2
+}
+declare namespace LogLevel {
+  function description(param: LogLevel): string ;
+  const keys: string[] ;
+}
+declare enum SortOrder {
+  decreasing = 1,
+  increasing = 0
+}
+declare namespace SortOrder {
+  function description(param: SortOrder): string ;
+  const keys: string[] ;
+}
+declare enum TextAlign {
+  center = 2,
+  justfied = 3,
+  left = 0,
+  normal = 4,
+  right = 1
+}
+declare namespace TextAlign {
+  function description(param: TextAlign): string ;
+  const keys: string[] ;
+}
+declare enum ValueType {
+  URLType = 13,
+  arrayType = 11,
+  boolType = 1,
+  colorType = 14,
+  dateType = 4,
+  dictionaryType = 10,
+  enumType = 9,
+  imageType = 15,
+  nullType = 0,
+  numberType = 2,
+  objectType = 17,
+  pointType = 6,
+  rangeType = 5,
+  recordType = 16,
+  rectType = 8,
+  segmentType = 18,
+  sizeType = 7,
+  stringType = 3
+}
+declare namespace ValueType {
+  function description(param: ValueType): string ;
+  const keys: string[] ;
+}
 /**
  * Builtin.d.ts
  */
@@ -277,6 +424,15 @@ interface TableIF {
 	toString(): 		string
 }
 
+interface MappingTableIF extends TableIF {
+
+	setFilterFunction(filter: (rec: RecordIF) => boolean): void ;
+	addVirtualField(field: string, callback: (rec: RecordIF) => any): void
+
+	sortOrder: 		SortOrder | null ;
+	setCompareFunction(compare: (rec0: RecordIF, rec1: RecordIF) => ComparisonResult): void
+}
+
 interface SymbolsIF {
 	characterA:		URLIF ;
 	chevronBackward:	URLIF ;
@@ -342,6 +498,7 @@ declare function ArrayInStorage(path: string, storage: StorageIF): ArrayIF | nul
 declare function SetInStorage(path: string, storage: StorageIF): SetIF | null ;
 declare function DictionaryInStorage(path: string, storage: StorageIF): DictionaryIF | null ;
 declare function TableInStorage(path: string, storage: StorageIF): TableIF | null ;
+declare function MappingTableInStorage(path: string, storage: StorageIF): MappingTableIF | null ;
 
 declare function isArray(value: any): boolean ;
 declare function isBitmap(value: any): boolean ;
@@ -430,6 +587,7 @@ interface Math {
     clamp(src: number, min: number, max: number): number;
 }
 declare function int(value: number): number;
+declare function compareNumbers(n0: number, n1: number): ComparisonResult;
 /**
  * Debug.ts
  */
@@ -524,133 +682,3 @@ declare class Turtle {
 /// <reference path="Process.d.ts" />
 /// <reference path="Enum.d.ts" />
 declare function requestContactAccess(): boolean;
-declare enum AccessType {
-  append = 2,
-  read = 0,
-  write = 1
-}
-declare namespace AccessType {
-  function description(param: AccessType): string ;
-  const keys: string[] ;
-}
-declare enum Alignment {
-  center = 3,
-  fill = 2,
-  leading = 0,
-  trailing = 1
-}
-declare namespace Alignment {
-  function description(param: Alignment): string ;
-  const keys: string[] ;
-}
-declare enum AnimationState {
-  idle = 0,
-  pause = 2,
-  run = 1
-}
-declare namespace AnimationState {
-  function description(param: AnimationState): string ;
-  const keys: string[] ;
-}
-declare enum Authorize {
-  authorized = 3,
-  denied = 2,
-  undetermined = 0
-}
-declare namespace Authorize {
-  function description(param: Authorize): string ;
-  const keys: string[] ;
-}
-declare enum Axis {
-  horizontal = 0,
-  vertical = 1
-}
-declare namespace Axis {
-  function description(param: Axis): string ;
-  const keys: string[] ;
-}
-declare enum Distribution {
-  equalSpacing = 3,
-  fill = 0,
-  fillEqually = 2,
-  fillProportinally = 1
-}
-declare namespace Distribution {
-  function description(param: Distribution): string ;
-  const keys: string[] ;
-}
-declare enum ExitCode {
-  commaneLineError = 2,
-  exception = 4,
-  internalError = 1,
-  noError = 0,
-  syntaxError = 3
-}
-declare namespace ExitCode {
-  function description(param: ExitCode): string ;
-  const keys: string[] ;
-}
-declare enum FileType {
-  directory = 2,
-  file = 1,
-  notExist = 0
-}
-declare namespace FileType {
-  function description(param: FileType): string ;
-  const keys: string[] ;
-}
-declare enum FontSize {
-  large = 19,
-  regular = 13,
-  small = 11
-}
-declare namespace FontSize {
-  function description(param: FontSize): string ;
-  const keys: string[] ;
-}
-declare enum LogLevel {
-  debug = 3,
-  detail = 4,
-  error = 1,
-  nolog = 0,
-  warning = 2
-}
-declare namespace LogLevel {
-  function description(param: LogLevel): string ;
-  const keys: string[] ;
-}
-declare enum TextAlign {
-  center = 2,
-  justfied = 3,
-  left = 0,
-  normal = 4,
-  right = 1
-}
-declare namespace TextAlign {
-  function description(param: TextAlign): string ;
-  const keys: string[] ;
-}
-declare enum ValueType {
-  URLType = 13,
-  arrayType = 11,
-  boolType = 1,
-  colorType = 14,
-  dateType = 4,
-  dictionaryType = 10,
-  enumType = 9,
-  imageType = 15,
-  nullType = 0,
-  numberType = 2,
-  objectType = 17,
-  pointType = 6,
-  rangeType = 5,
-  recordType = 16,
-  rectType = 8,
-  segmentType = 18,
-  sizeType = 7,
-  stringType = 3
-}
-declare namespace ValueType {
-  function description(param: ValueType): string ;
-  const keys: string[] ;
-}
---
+++ Resource/Sample/storage-map-table0.jspkg/main.ts
@@ -0,0 +1,63 @@
+/*
+ * main.ts
+ */
+
+/// <reference path="../types/KiwiLibrary.d.ts"/>
+
+function main(args : [string])
+{
+	console.print("# storage-mapping-0\n") ;
+	let result = 0 ;
+
+	let storage = Storage("storage") ;
+	if(storage == null){
+		console.print("Failed to allocate storage\n") ;
+		return -1 ;
+	}
+
+	/* allocate mapping table */
+	let table = MappingTableInStorage("members", storage) ;
+	if(table == null){
+		console.print("Failed to allocate table\n") ;
+		return -1 ;
+	}
+
+	console.print("***** Initial state\n") ;
+	console.print("recordCount = " + table.recordCount + "\n") ;
+	if(table.recordCount != 5){
+		console.print("Unexpected record count\n") ;
+		return -1 ;
+	}
+	printTable(table) ;
+
+	console.print("***** Set for sorting\n") ;
+	table.sortOrder = SortOrder.increasing ;
+	table.setCompareFunction(function(rec0, rec1) {
+		let pid0 = rec0.value("pid") ;
+		let pid1 = rec1.value("pid") ;
+		return compareNumbers(pid0, pid1) ;
+	})
+	table.setFilterFunction(function(rec) {
+		return rec.value("pid") > 0 ;
+	})
+
+	printTable(table) ;
+
+	return result ;
+}
+
+function printTable(table: TableIF): number
+{
+	let result = 0 ;
+	let count  = table.recordCount ;
+	for(let i=0 ; i<count ; i++){
+		let rec = table.record(i) ;
+		if(rec != null){
+			console.print(rec.toString() + "\n") ;
+		} else {
+			console.print("Failed to get record: " + i + "\n") ;
+		}
+	}
+	return result ;
+}
+
